31d4747ac26841cff87858d7f23ccf9b0db40d01
bingoohuang <bingoo.huang@gmail.com>
2015-05-18 11:29:39 +0800

    add tcode parsing from domain

--- lua/hi_tcode.lua
+++ lua/hi_tcode.lua
@@ -17,34 +17,48 @@ local _M = {
         round           : AES encrypt round, default 5
 --]]
 function _M.tcode (self, opt)
-    local tcode = string.match(ngx.var.uri, "/tcode/(%w+)")
+    -- http://wiki.nginx.org/HttpCoreModule#.24host
+    -- This variable is equal to line Host in the header of request or name
+    -- of the server processing the request if the Host header is not available.
+    -- This variable may have a different value from $http_host in such cases:
+    -- 1) when the Host input header is absent or has an empty value,
+    --    $host equals to the value of server_name directive;
+    -- 2)when the value of Host contains port number,
+    --    $host doesn't include that port number.
+    -- $host's value is always lowercase since 0.8.17.
+
+    -- for lua Patterns, refer to http://www.lua.org/pil/20.2.html
+    local tcode = string.match(ngx.var.host, "(%w+)%.easy%-hi%.cn")
+    local domain = tcode or false
+    tcode = tcode or string.match(ngx.var.uri, "^/(%w+)$")
+    tcode = tcode or string.match(ngx.var.uri, "/tcode/(%w+)")
+
+    local hi_aes = require("hi_aes"):new(opt)
+    
     if tcode then
         local hi_redis = require("hi_redis"):connect(opt)
         local tid = hi_redis:get(tcode)
         hi_redis:close()
 
         if not tid then ngx.exit(404) return end
+        ngx.req.set_header("tid", tid)
+
+        if not domain then
+            require("hi_cookie"):set({
+                key = "tid", path = "/",
+                value = hi_aes:encrypt(tid)
+            })
+        end
+
+        return tid
+    else
+        local encrptedTid = ngx.var.cookie_tid
+        if not encrptedTid then ngx.exit(404) return end
+
+        local tid = hi_aes:decrypt(encrptedTid)
         ngx.req.set_header("tid" , tid)
 
-        local hi_aes = require("hi_aes"):new(opt)
-        require("hi_cookie"):set(
-            { key = "tid",
-              value = hi_aes:encrypt(tid),
-              path = "/"
-            }
-        )
         return tid
-
-    else
-        local tid = ngx.var.cookie_tid
-        if not tid then ngx.exit(404) return end
-
-        local hi_aes = require("hi_aes"):new(opt)
-        local clearTid = hi_aes:decrypt(tid)
-        ngx.req.set_header("tid" , clearTid)
-
-        return clearTid
-
     end
 end
 
