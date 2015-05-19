b49da4cac23cb04978733e9e8dcef56c48333e11
bingoohuang <bingoo.huang@gmail.com>
2015-05-19 22:01:59 +0800

    refactor for tcode prevWord

--- lua/hi_tcode.lua
+++ lua/hi_tcode.lua
@@ -4,34 +4,27 @@ local _M = {
 
 --[[
     opt could contains:
+        prevWord        : for tcode identifaction previous word in url,
+                          default "tcode", like /abc/tcode/T013323/index.
+        host            : redis host, default "127.0.0.1".
+        port            : redis port, default 6379.
+        timeout         : redis connect timeout, default 3s.
+        maxIdleTimeout  : redis keep_alive timeout, default 10s.
+        poolSize        : redis connect pool size, default 10.
+        auth            : redis auth, default nil.
 
-        host            : redis host, default "127.0.0.1"
-        port            : redis port, default 6379
-        timeout         : redis connect timeout, default 3s
-        maxIdleTimeout  : redis keep_alive timeout, default 10s
-        poolSize        : redis connect pool size, default 10
-        auth            : redis auth, default nil
-
-        key             : AES encrypt key, default "AKeyForAES-256-CBC"
-        salt            : AES encrypt salt, default "HI_SALT"
-        round           : AES encrypt round, default 5
+        key             : AES encrypt key, default "AKeyForAES-256-CBC".
+        salt            : AES encrypt salt, default "HI_SALT".
+        round           : AES encrypt round, default 5.
 --]]
-function _M.tcode (self, opt)
+_M.tcode = function(opt)
     -- http://wiki.nginx.org/HttpCoreModule#.24host
-    -- This variable is equal to line Host in the header of request or name
-    -- of the server processing the request if the Host header is not available.
-    -- This variable may have a different value from $http_host in such cases:
-    -- 1) when the Host input header is absent or has an empty value,
-    --    $host equals to the value of server_name directive;
-    -- 2)when the value of Host contains port number,
-    --    $host doesn't include that port number.
-    -- $host's value is always lowercase since 0.8.17.
-
     -- for lua Patterns, refer to http://www.lua.org/pil/20.2.html
-    local tcode = string.match(ngx.var.host, "(%w+)%.easy%-hi%.cn")
+    local tcode = string.match(ngx.var.host, "^(%w+)%.[%w%-]+%.%w+$")
     local domain = tcode or false
     tcode = tcode or string.match(ngx.var.uri, "^/(%w+)$")
-    tcode = tcode or string.match(ngx.var.uri, "/tcode/(%w+)")
+    local prevWord = opt and opt.prevWord or "tcode"
+    tcode = tcode or string.match(ngx.var.uri, "/" .. prevWord .. "/(%w+)")
 
     if tcode then
         local hi_redis = require("hi_redis"):connect(opt)
@@ -39,13 +32,15 @@ function _M.tcode (self, opt)
         hi_redis:close()
 
         if not tid then ngx.exit(404) return end
+
         ngx.req.set_header("tid", tid)
+        ngx.req.set_header("tcode", tcode)
 
         if not domain then
             local hi_aes = require("hi_aes"):new(opt)
             require("hi_cookie"):set({
                 key = "tid", path = "/",
-                value = hi_aes:encrypt(tid)
+                value = hi_aes:encrypt(tcode .. "^" .. tid)
             })
         end
 
@@ -55,8 +50,11 @@ function _M.tcode (self, opt)
         if not encrptedTid then ngx.exit(404) return end
 
         local hi_aes = require("hi_aes"):new(opt)
-        local tid = hi_aes:decrypt(encrptedTid)
-        ngx.req.set_header("tid" , tid)
+        local tcodeAndTid = hi_aes:decrypt(encrptedTid)
+        local tcode, tid = string.match(tcodeAndTid, "(%w+)^(%w+)")
+
+        ngx.req.set_header("tid", tid)
+        ngx.req.set_header("tcode", tcode)
 
         return tid
     end
